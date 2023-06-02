1a5a29da764e0787beb337b2736834f313336f54
Dominykas Grigaliunas <drigaliunas@gmail.com>
2023-06-02 19:04:05 +0300

    created models

---
+++ conference_app/conferences/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# a.Konferencija – turi datą nuo-iki, pavadinimą, [sukūrimo
+# ir modifikavimo datos (auto_now, auto_now_add)]
+
+class Conference(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    title = models.CharField(max_length = 100)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+

e50beaa70b1fc859e03532dff15a9fb7526c833e
Dominykas Grigaliunas <drigaliunas@gmail.com>
2023-06-05 18:36:38 +0300

    create event detail view

--- conference_app/events/views.py
+++ conference_app/events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from events.models import Event
+
 
 # Create your views here.
+class EventDetailView(DetailView):
+    model = Event
+    context_object_name = "renginys"
